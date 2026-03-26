1f00b7692d13f01f4aea47d9328a8d4fc93b6699
Voltline <44248193@qq.com>
2026-03-26 14:46:31 +0800

    init

---
+++ KagiPeek/KagiPeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  KagiPeekApp.swift
+//  KagiPeek
+//
+//  Created by Voltline on 2026/3/26.
+//
+
+import SwiftUI
+
+@main
+struct KagiPeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
